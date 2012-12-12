d2dbc36cbb764b943b51a334e52d1633d34c8a4b
Daniel Widegren <daniel@widegren.org>
2012-12-12 11:18:45 +0100

    First push with base project contains no code yet.

---
+++ WaniKaniClient/WKClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WaniKaniClient
+{
+    public class WKClient
+    {
+        public string APIKey { get; private set; }
+
+        public WKClient(string apiKey)
+        {
+            APIKey = apiKey;
+        }
+    }
+}
