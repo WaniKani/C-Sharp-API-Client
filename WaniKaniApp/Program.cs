7cb2349b2965afebeb7fa619d4d362660af17d82
Daniel Widegren <daniel@widegren.org>
2012-12-20 09:58:30 +0100

    Renamed the demo project to WaniKaniClientDemo

--- WaniKaniApp/Program.cs
+++ WaniKaniApp/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using WaniKaniClientLib;
 
-namespace WaniKaniApp
+namespace WaniKaniClientDemo
 {
     class Program
     {
--- WaniKaniApp/Properties/AssemblyInfo.cs
+++ WaniKaniApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WaniKaniApp")]
+[assembly: AssemblyTitle("WaniKaniClientDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WaniKaniApp")]
+[assembly: AssemblyProduct("WaniKaniClientDemo")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
