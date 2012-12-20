328688368a929202f3357b60b4cb24c5ce9f67b0
Daniel Widegren <daniel@widegren.org>
2012-12-20 12:36:24 +0100

    Removed my own API key... Don't bother try it as I changed it.

--- WaniKaniApp/Program.cs
+++ WaniKaniApp/Program.cs
@@ -10,10 +10,12 @@ namespace WaniKaniClientDemo
     {
         static void Main(string[] args)
         {
-            WaniKaniClient client = new WaniKaniClient("114023cc08517f6180a1f120bfffe87f");
+            Console.WriteLine("PLease enter your API key: ");
+            string apiKey = Console.ReadLine();
+
+            WaniKaniClient client = new WaniKaniClient(apiKey);
 
             var data2 = client.UserInformation;
-
             var data = client.Vocabulary();
 
             Console.WriteLine("Please a key to exit");
