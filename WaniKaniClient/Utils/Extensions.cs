bdda36156a3deaa084b7d08e178425005424d12c
Iris Ward <dan@wardy.org>
2015-08-22 18:09:44 +0100

    Fix unix epoch
    Brain fart - the unix epoch is 1970/1/1, not 0/0/0, which is an invalid
    date and raises an exception...

--- WaniKaniClient/Utils/Extensions.cs
+++ WaniKaniClient/Utils/Extensions.cs
@@ -4,6 +4,6 @@ namespace WaniKaniClientLib.Utils
 {
     internal static class Utilities
     {
-        public static DateTime UnixEpoch = new DateTime(0, 0, 0, 0, 0, 0, 0, DateTimeKind.Utc);
+        public static DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
     }
 }
